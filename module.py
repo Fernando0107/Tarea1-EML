08e5424626b7a95c6c8d855b70cb99c1694d01b1
Fernando González <rmfernando@outlook.com>
2020-01-13 23:14:21 -0600

    Updates...

--- module.py
+++ module.py
@@ -1,6 +1,10 @@
 menu = {"1. Sumar": lambda x, y: x + y, "2. Restar" : lambda x, y: x - y}
 
 def gen_menu(dic):
+    """
+    gen_menu:
+        Funcion que genera menus de manera dinamica.
+    """
 
     print("   --- Menu ---")
 
@@ -10,7 +14,7 @@ def gen_menu(dic):
         #print(menu[list(dic.keys())[1]](1,2))
     x = int(input("Ingrese una opción del menú: "))
     
-    if x not in list(range(1, len(dic.items()) + 1)):
+    if x not in list(range(1, len(dic.items()) + 1)): 
         print("Opción no valida.")
     else:
         print(menu[list(dic.keys())[x - 1]](1, 2))
--- triangle.py
+++ triangle.py
@@ -1,6 +1,6 @@
 file = open("triangle.txt", "r")
 x = []
-temporal = 0
+temporal = []
 
 for line in file:
 
@@ -11,22 +11,19 @@ for i in range(len(x)):
 
     for j in range(len(x[i])):
 
-        if (int(x[i][j + 1]) > int(x[i + 1][j + 1])):
-            temporal += int(x[i][j + 1])
-        else:
-            temporal += int(x[i + 1][j + 1])
+        try:
+            if (int(x[i][j]) > int(x[i + 1][j + 1])):
+                temporal.append(int(x[i][j]))
+            else:
+                temporal.append(int(x[i + 1][j + 1]))
 
+        except:
+            pass
 
-print(temporal)
+total = sum(temporal)
+print(total)
+
 
 """
-if (h == 0):
-            h += int(x[i][j])
-
-        if (len(x[i]) != 0 | i == len(x)):
-            if (int(x[i][j]) > int(x[i + 1][j])):
-                h += int(x[i][j])
-            else:
-                h += int(x[i+1][j])
-# print(x[99][0])
+x[posicion de arrays][valor dentro del array]
 """
