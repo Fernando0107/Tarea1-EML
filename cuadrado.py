a00b244b980fcb63b6623dc50be742f297c23115
Fernando González <rmfernando@outlook.com>
2020-01-13 22:34:34 -0600

    updates...

--- cuadrado.py
+++ cuadrado.py
@@ -1,37 +1,52 @@
 import math
 
-A = (0,0)
-B = (0,0)
-C = (0,0)
-D = (0,0)
+A = (-2, 9)
+B = (4, 6)
+C = (1, 0)
+D = (-5, 3)
+
 
 def cuadrado(
         a, b,
         c, d):
 
+    """
+    cuadrado: 
+        Funcion que recibe cuatro parametros de tipo tupla.
+        Esta funcion calcula distancias entre puntos con 
+        ayuda de una funcion auxiliar llamada dist.
+        Esta funcion retorna si dadas estas 4 tuplas, cumplen
+        con ser un cuadrado.
+    """
     z = dist(a, b)
     x = dist(a, c)
     z1 = dist(a, d)
 
     if z == z1:
 
-        x1 = dist(b,d)
+        x1 = dist(b, d)
 
         if(x1 == x):
             return("Es un cuadrado")
         else:
             return("No es un cuadrado.")
-    else: 
+    else:
         return("No es un cuadrado")
 
-
 def dist(a, b):
+
+    """
+    dist:
+        Funcion que calcula la distancia entre 2 puntos.
+        Recibe 2 parametros: 2 tuplas.
+    """
 
     d = math.sqrt(
         ((a[0] - b[0])**(2))
         + ((a[1] - b[1])**2)
-            )
+    )
 
     return d
 
-print(cuadrado(A,B,C,D))
+
+print(cuadrado(A, B, C, D))
--- triangle.py
+++ triangle.py
@@ -1,5 +1,32 @@
 file = open("triangle.txt", "r")
+x = []
+temporal = 0
 
 for line in file:
-    
-    print(line[:2])
+
+    x.append(line.split())
+
+
+for i in range(len(x)):
+
+    for j in range(len(x[i])):
+
+        if (int(x[i][j + 1]) > int(x[i + 1][j + 1])):
+            temporal += int(x[i][j + 1])
+        else:
+            temporal += int(x[i + 1][j + 1])
+
+
+print(temporal)
+
+"""
+if (h == 0):
+            h += int(x[i][j])
+
+        if (len(x[i]) != 0 | i == len(x)):
+            if (int(x[i][j]) > int(x[i + 1][j])):
+                h += int(x[i][j])
+            else:
+                h += int(x[i+1][j])
+# print(x[99][0])
+"""
