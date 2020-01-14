3d254dab38fd326b8775e3782553a7fb63f4d7d2
Fernando González <rmfernando@outlook.com>
2020-01-14 15:06:06 -0600

    menu updated.

--- module.py
+++ module.py
@@ -1,11 +1,24 @@
-menu = {"1. Sumar": lambda x, y: x + y, "2. Restar": lambda x, y: x - y,
-        "3. Multiplicar": lambda x, y: x * y}
+menu = {
+    "1. Sumar": lambda x, y: x + y,
+    "2. Restar": lambda x, y: x - y,
+    "3. Multiplicar": lambda x, y: x * y
+}
 
-print("--- Menu ---\n" + "\n".join("{}".format(k) for k, v in menu.items()))
 
-x = int(input("Ingrese una opción del menú: "))
+def gen_menu(dic):
+    """
+    gen_menu:
+        Funcion que genera menus de manera dinamica.
+    """
+    print("--- Menu ---\n" + "\n".join("{}".format(k)
+                                       for k, v in menu.items()))
 
-if x not in list(range(1, len(menu.items()) + 1)):
-    print("Opción no valida.")
-else:
-    print(menu[list(menu.keys())[x - 1]](1, 2))
+    x = int(input("Ingrese una opción del menú: "))
+
+    if x not in list(range(1, len(dic.items()) + 1)):
+        return("Opción no valida.")
+    else:
+        return(menu[list(dic.keys())[x - 1]](1, 2))
+
+
+print(gen_menu(menu))
